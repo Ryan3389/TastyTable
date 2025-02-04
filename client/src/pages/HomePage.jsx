e91fc1139e7a0eba5d926ea965a76945e1fa9679
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-02-04 16:46:16 -0500

    added search results

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -7,8 +7,7 @@ function HomePage() {
                     <h1>Discover Delicious <br /> Recipes in Seconds!</h1>
                     <p>Search recipes, filter by category, or let us surprise you!</p>
                     <span className="main-buttons">
-                        <Link to='/search'>Find Recipes</Link>
-                        <Link to='/random'>Surpise Me</Link>
+                        <Link to='/search'>Search Recipes</Link>
                     </span>
                 </article>
             </div>
--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -4,12 +4,16 @@ function SearchPage() {
         recipeName: ""
     })
 
+    const [recipeData, setRecipeData] = useState([])
+
     const handleChange = (e) => {
         const { name, value } = e.target
         setSearchState({
             ...searchState,
             [name]: value
         })
+
+        console.log(searchState)
     }
 
     const handleFormSubmit = async (e) => {
@@ -28,8 +32,14 @@ function SearchPage() {
             }
 
             const data = await response.json()
+            const searchResults = data.searchResults.meals.map((recipe) => ({
+                recipeTitle: recipe.strMeal,
+                recipeImg: recipe.strMealThumb,
+                recipeLink: recipe.strSource
+            }))
 
-            console.log(data)
+            setRecipeData(searchResults)
+
 
         } catch (error) {
             console.error(error)
@@ -37,20 +47,31 @@ function SearchPage() {
     }
     return (
         <section className="category-section">
+
             <span className="search-container">
 
                 <form onSubmit={handleFormSubmit}>
-                    <label htmlFor="search">Search for your favourite recipes</label>
+                    <label htmlFor="recipeName">Search for your favourite recipes</label>
                     <input
                         type="text"
-                        name="search"
+                        name="recipeName"
                         id="search"
                         onChange={handleChange}
                         aria-label="search recipes" />
                     <input type="submit" value="search" />
 
                 </form>
+
             </span>
+            {recipeData && recipeData.map((recipe, index) => (
+                <article className="recipe-card" key={index}>
+                    <p>{recipe.recipeTitle}</p>
+                    <img src={recipe.recipeImg} className="recipe-img" alt={recipe.recipeTitle} />
+                    <a target="_blank" href={recipe.recipeLink}>
+                        <p>Check it out</p>
+                    </a>
+                </article>
+            ))}
         </section>
     )
 }
